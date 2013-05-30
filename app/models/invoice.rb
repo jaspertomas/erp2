ba51552fa562557313eb761f76544ff1d50eef15
Jasper Tomas <jaspertomas@gmail.com>
2013-05-30 12:40:00 +0800

    started working on invoice and purchase input forms

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,8 @@
 class Invoice < ActiveRecord::Base
   attr_accessible :balance, :cash, :checkcleardate, :checkcollectevents, :cheque, :chequeamt, :chequedata, :chequedate, :credit, :customer_id, :customer_name, :date, :datepaid, :discamt, :discrate, :dsrdeduction, :duedate, :invno, :payonly, :ponumber, :salesman_id, :saletype, :technician_id, :template_id, :terms_id, :total
+  validates_date :chequedate
+  validates_date :date
+  validates_date :duedate
+  validates_date :datepaid
+  
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,8 @@
 class Purchase < ActiveRecord::Base
   attr_accessible :balance, :cash, :cheque, :chequedata, :chequedate, :chequeno, :credit, :date, :datereceived, :discamt, :discrate, :duedate, :employee_id, :invno, :pono, :status, :tax, :template_id, :terms_id, :total, :type, :vendor_id, :vendor_invoice, :vendor_name
+  validates_date :chequedate
+  validates_date :date
+  validates_date :duedate
+  validates_date :datereceived
+  
 end
