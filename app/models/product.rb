779bfffa585887d783da7ab5acd5ec27ca978d05
Jasper Tomas <jaspertomas@gmail.com>
2013-06-02 20:03:38 +0800

    worked on invoice

--- app/models/product.rb
+++ app/models/product.rb
@@ -27,4 +27,5 @@
 
 class Product < ActiveRecord::Base
   attr_accessible :barcode, :brand_id, :category1, :category10, :category2, :category3, :category4, :category5, :category6, :category7, :category8, :category9, :is_service, :monitored, :name, :producttype_id, :publish, :qty
+  belongs_to :producttype, :class_name => "ProductType", :foreign_key => 'producttype_id'
 end
